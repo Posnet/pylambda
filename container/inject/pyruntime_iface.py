b193f450b2b8480cae5d79ce02e88888851093ab
Alec Posney <alec.posney@gmail.com>
2018-02-17 22:15:17 +1100

    autosave

---
+++ container/inject/pyruntime_iface.py
@@ -0,0 +1,62 @@
+def init_runtime() -> None:
+ pass
+
+
+def get_remaining_time() -> int:
+ pass
+
+
+def log_bytes(message: str, fileno: int) -> None:
+ pass
+
+def log_sb(message: str) -> None:
+ pass
+
+def send_console_message(message: str, length: int) -> None:
+ pass
+
+
+from typing import Dict
+credential_type = Dict[str, str]
+context_type = Dict[str, str]
+
+def receive_invoke() -> (str, int, credential_type, str, context_type, str, str):
+ pass
+
+def receive_start() -> (str, str, str, int, credential_type):
+ pass
+
+
+
+def report_user_init_start() -> None:
+ pass
+
+def report_user_init_end() -> None:
+ pass
+
+
+def report_user_invoke_start() -> None:
+ pass
+
+def report_user_invoke_end() -> None:
+ pass
+
+
+
+
+def report_fault(invoke_id: str,
+                 message: str,
+                 exception: str,
+                 backtrace: str) -> None:
+ pass
+
+def report_running(invoke_id: str) -> None:
+ pass
+
+def report_done(invoke_id: str, errortype: str, result: str) -> None:
+ 
+ pass
+
+
+report_xray_exception(xray_json: str) -> None:
+ pass
