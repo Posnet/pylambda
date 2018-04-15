aea949b0135f16fa5cfac70693d124bbf119208b
Alec Posney <alec.posney@gmail.com>
2018-04-15 20:03:29 +1000

    more cleanup

--- container/handler.py
+++ container/handler.py
@@ -1,16 +1,15 @@
-import ctypes
 import sys
-from inject.runtime_types import *
+from inject.runtime_types import get_native_runtime_struct
 
 
 def pyhandler(event, context):
     print(event, context)
     return {'working': True}
 
+
 def handler(event, context):
 
     print(sys.path)
     print(get_native_runtime_struct())
-    
+
     return "Failed to inject"
-    
